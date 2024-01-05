3845ae25ab0ac1000a303b5a6431cce036ed9474
Kiril Kapustin <kiril.kapustin@gmail.com>
2024-01-05 15:24:15 +0200

    refactoring last changes

--- constants.js
+++ constants.js
@@ -10,7 +10,7 @@ module.exports = {
     TYPE_KING: 'king',
     TYPE_QUEEN: 'queen',
 
-    DEFAULT_POSITIONS: {
+    _DEFAULT_POSITIONS: {
         '1': {
             '1': {type: 'rook', color: 'white', moved: false},
             '5': {type: 'king', color: 'white', moved: false},
@@ -21,16 +21,16 @@ module.exports = {
         }
     },
 
-    _DEFAULT_POSITIONS: {
+    DEFAULT_POSITIONS: {
         '1': {
-            '1': {type: 'rook', color: 'white'},
+            '1': {type: 'rook', color: 'white', moved: false},
             '2': {type: 'knight', color: 'white'},
             '3': {type: 'bishop', color: 'white'},
             '4': {type: 'queen', color: 'white'},
-            '5': {type: 'king', color: 'white'},
+            '5': {type: 'king', color: 'white', moved: false},
             '6': {type: 'bishop', color: 'white'},
             '7': {type: 'knight', color: 'white'},
-            '8': {type: 'rook', color: 'white'}
+            '8': {type: 'rook', color: 'white', moved: false}
         },
         '2': {
             '1': {type: 'pawn', color: 'white'},
@@ -53,14 +53,14 @@ module.exports = {
             '8': {type: 'pawn', color: 'black'}
         },
         '8': {
-            '1': {type: 'rook', color: 'black'},
+            '1': {type: 'rook', color: 'black', moved: false},
             '2': {type: 'knight', color: 'black'},
             '3': {type: 'bishop', color: 'black'},
             '4': {type: 'queen', color: 'black'},
-            '5': {type: 'king', color: 'black'},
+            '5': {type: 'king', color: 'black', moved: false},
             '6': {type: 'bishop', color: 'black'},
             '7': {type: 'knight', color: 'black'},
-            '8': {type: 'rook', color: 'black'}
+            '8': {type: 'rook', color: 'black', moved: false}
         }
     },
 
