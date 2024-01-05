3e299e561dfe12bc2d6f588327c5bbd3012990ab
Kiril Kapustin <kiril.kapustin@gmail.com>
2024-01-05 15:30:41 +0200

    refactoring last changes

--- helpers.js
+++ helpers.js
@@ -3,38 +3,4 @@ module.exports = {
     cloneObject: function(obj) {
         return JSON.parse(JSON.stringify(obj));
     },
-    generateRandomString: function(length, useChars) {
-        let characters = '';
-
-        if(useChars.includes('num')) {
-            characters += '0123456789'
-        }
-
-        if(useChars.includes('lwr')) {
-            characters += 'abcdefghijklmnopqrstuvwxyz'
-        }
-
-        if(useChars.includes('upr')) {
-            characters += 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
-        }
-
-        let result = '';
-        for (let i = 0; i < length; i++) {
-            const randomIndex = Math.floor(Math.random() * characters.length);
-            result += characters.charAt(randomIndex);
-        }
-
-        return result;
-    },
-    logit(data) {
-        fs.writeFileSync('log.txt', JSON.stringify(data))
-    },
-    rspn(data, success) {
-        return {
-            success,
-            data
-        }
-    }
-
-
 };
